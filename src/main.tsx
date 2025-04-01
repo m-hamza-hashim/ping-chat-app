b12fc9c37229f0bb850be2122c1fe8f6ac7b262a
m.hamza hashim <m.hamzahashim1@gmail.com>
2025-04-02 00:12:26 +0500

    complete

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
